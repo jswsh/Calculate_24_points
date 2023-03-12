732265c173cc922616dda58346c470838c170ad3
jsw <jiashuwei_sh@163.com>
2023-03-12 20:39:31 +0800

    初步计算ok
    未处理自由输入数字

--- c24p.py
+++ c24p.py
@@ -33,13 +33,64 @@ for i,x1 in enumerate(nums):
                 i_count += 1
                 #print(str(i_count) + " -- " + str(idx_arr) + ":"+str(nums_arr))
                 listNums.append(nums_arr)
-print(listNums) 
-print(len(listNums))  
-#print(listNums[0])  
-#print(listNums[0][0])
+#print(listNums) 
+#print(len(listNums))  
+
 for i,a in enumerate(listNums):
-    x = a[0]+a[1]+a[2]+a[3]
-    print(str(a) + " = " + str(x))
+#for i,a in enumerate([listNums[0]]):
+    s = ""
+    x = 0
+    
+    for c1 in ["-","+","/","*"]:
+        #print(c1)
+        #if c==0 :
+        s1 = str(a[0])+c1+str(a[1]) 
+        if c1 == "-":
+            x1 = a[0]-a[1]
+        elif c1 == "+":
+            x1 = a[0]+a[1]
+        elif c1 == "/":
+            x1 = a[0]/a[1]
+        elif c1 == "*":
+            x1 = a[0]*a[1]
+
+        for c2 in ["-","+","/","*"]:
+            #print(c2)
+            #if c==0 :
+            s2 = s1+c2+str(a[2]) 
+            
+            if c2 == "-":
+                x2 = x1-a[2]
+            elif c2 == "+":
+                x2 = x1+a[2]
+            elif c2 == "/":
+                x2 = x1/a[2]
+            elif c2 == "*":
+                x2 = x1*a[2]
+
+            for c3 in ["-","+","/","*"]:
+                #print(c3)
+                #if c==0 :
+                s3 = s2+c3+str(a[3])
+
+                if c3 == "-":
+                    x3 = x2-a[3]
+                elif c3 == "+":
+                    x3 = x2+a[3]
+                elif c3 == "/":
+                    x3 = x2/a[3]
+                elif c3 == "*":
+                    x3 = x2*a[3]
+
+                s = s3 + "=" +str(x3)
+
+                if x3 == 24:
+                    print(s)
+                
+ 
+         
+                
+    #print(s)
 
 #listNums.append(nums)
 
